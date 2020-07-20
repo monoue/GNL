95e93f9fef44b1aa596a02eff0b12f28531e591e
Makoto Onoue <monoue@student.42tokyo.jp>
2020-07-20 17:38:37 +0900

    practicing

--- myown/get_next_line.c
+++ myown/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/17 15:14:11 by monoue            #+#    #+#             */
-/*   Updated: 2020/07/17 17:28:43 by monoue           ###   ########.fr       */
+/*   Updated: 2020/07/20 16:39:13 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		get_new_line_index(char *str)
+static int		get_index_of_new_line(char *str)
 {
 	size_t	i;
 
@@ -31,108 +31,77 @@ static int		free_and_return_error(char **stack, char *buff)
 	return (ERROR);
 }
 
-static int		get_to_new_line(int fd, char **stack, int *nl_index)
+static int		get_until_new_line(int fd, char **stack, int *nl_index)
 {
 	char	*buff;
 	char	*tmp;
-	int		ret;
+	int		read_ret;
 
-	if (!(buff = malloc(sizeof(*buff) * BUFFER_SIZE + 1)))
+	if (!(buff = malloc(sizeof(*buff) * (BUFFER_SIZE + 1))))
 		return (ERROR);
-	while ((ret = read(fd, buff, BUFFER_SIZE)) > 0)
+	while ((read_ret = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
-		buff[ret] = '\0';
+		buff[read_ret] = '\0';
 		tmp = ft_strjoin(*stack, buff);
 		free(*stack);
 		if (!tmp)
 			return (free_and_return_error(stack, buff));
 		*stack = tmp;
-		if ((*nl_index = get_new_line_index(*stack)) != NOT_FOUND)
+		if ((*nl_index = get_index_of_new_line(*stack)) != NOT_FOUND)
 			break ;
 	}
-	if (ret == FILE_END)
-		*nl_index = get_new_line_index(*stack);
+	if (read_ret == FILE_END)
+		*nl_index = get_index_of_new_line(*stack);
 	free(buff);
-	if (ret > 0)
+	if (read_ret > 0)
 		return (NOT_EOF);
-	return (ret);
+	return (read_ret);
 }
 
 int				get_next_line(int const fd, char **line)
 {
-	static char	*stack_arr[MAX_FD + 1];
-	char		*tmp;
-	int			ret;
+	static char	*fd_arr[MAX_FD + 1];
+	char		*fd_src;
+	int			read_status;
 	int			nl_index;
 
 	if (fd > MAX_FD || fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (ERROR);
-	if (!stack_arr[fd] && !(stack_arr[fd] = ft_strdup("")))
+	if (!fd_arr[fd] && !(fd_arr[fd] = ft_strdup("")))
 		return (ERROR);
-	ret = get_to_new_line(fd, &stack_arr[fd], &nl_index);
-	if (ret == ERROR)
+	read_status = get_until_new_line(fd, &fd_arr[fd], &nl_index);
+	if (read_status == ERROR)
 		return (ERROR);
-	if (ret == FILE_END && nl_index == NOT_FOUND)
+	if (read_status == FILE_END && nl_index == NOT_FOUND)
 	{
-		*line = ft_strdup(stack_arr[fd]);
-		free(stack_arr[fd]);
-		stack_arr[fd] = NULL;
+		*line = ft_strdup(fd_arr[fd]);
+		free(fd_arr[fd]);
+		fd_arr[fd] = NULL;
 		return (*line ? FILE_END : ERROR);
 	}
-	*line = ft_substr(stack_arr[fd], 0, nl_index);
-	tmp = ft_strdup(stack_arr[fd] + nl_index + 1);
-	free(stack_arr[fd]);
-	stack_arr[fd] = tmp;
-	return ((stack_arr[fd] && *line) ? NOT_EOF : FILE_END);
+	*line = ft_substr(fd_arr[fd], 0, nl_index);
+	fd_src = ft_strdup(fd_arr[fd] + nl_index + 1);
+	free(fd_arr[fd]);
+	fd_arr[fd] = fd_src;
+	return ((fd_arr[fd] && *line) ? NOT_EOF : FILE_END);
 }
 
-// #include <fcntl.h>
-// #include <stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-// int		main(int argc, char **argv)
-// {
-// 	char	*line;
-// 	int		ret;
-// 	int		fd;
+int		main(int argc, char **argv)
+{
+	char	*line;
+	int		ret;
+	int		fd;
 
-// 	fd = open(argv[1], O_RDONLY);
-// 	while ((ret = get_next_line(fd, &line)) >= 0)
-// 	{
-// 		printf("%s\n", line);
-// 		free(line);
-// 		if (ret == FILE_END)
-// 			break ;
-// 	}
-// 	close(fd);
-// }
-
-// static int	read_to_nl(int fd, char **stack_fd, int *index)
-// {
-// 	char	*buff;
-// 	char	*tmp;
-// 	int		ret;
-
-// 	if (!(buff = malloc(sizeof(*buff) * (BUFFER_SIZE + 1))))
-// 		return (ERROR);
-// 	while ((ret = read(fd, buff, BUFFER_SIZE)) > 0)
-// 	{
-// 		buff[ret] = '\0';
-// 		tmp = ft_strjoin(*stack_fd, buff);
-// 		free(*stack_fd);
-// 		if (!tmp)
-// 		{
-// 			free(stack_fd);
-// 			free(buff);
-// 			return (ERROR);
-// 		}
-// 		*stack_fd = tmp;
-// 		if ((index = get_new_line_index(*stack_fd)) != NOT_FOUND)
-// 			break ;
-// 	}
-// 	free(buff);
-// 	if (ret == FILE_END)
-// 		*index = get_new_line_index(*stack_fd);
-// 	if (ret > 0)
-// 		return (NOT_EOF);
-// 	return (ret);
-// }
+	fd = open(argv[1], O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) >= 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		if (ret == FILE_END)
+			break ;
+	}
+	close(fd);
+}
