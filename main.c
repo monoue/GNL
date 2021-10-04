bcc08c32dfbc3776c4fae0834bc1378f0d5c38ec
monoue <like_a_newborn_baby@icloud.com>
2021-10-04 14:57:34 +0900

    remove: unnecessary lines and files

--- main.c
+++ main.c
@@ -11,8 +11,7 @@ int	main(int argc, char **argv)
 	(void)argv;
 	write(1, "\n", 1);
 
-	// fd[0] = open("lorem.txt", O_RDONLY);
-	fd[0] = open("cats.txt", O_RDONLY);
+	fd[0] = open("lorem.txt", O_RDONLY);
 	len = 6;
 	while ((ret = get_next_line(fd[0], &line) > 0) && len-- > 0)
 	{
@@ -20,33 +19,5 @@ int	main(int argc, char **argv)
 		free(line);
 	}
 	free(line);
-	// fd[1] = open("cicero.txt", O_RDONLY);
-	// len = 6;
-	// while ((ret = get_next_line(fd[1], &line) > 0) && len-- > 0)
-	// {
-	// 	printf("fd: %d	%s\n", fd[1], line);
-	// 	free(line);
-	// }
-	// free(line);
-	// fd[2] = open("kafka.txt", O_RDONLY);
-	// len = 6;
-	// while ((ret = get_next_line(fd[2], &line) > 0) && len-- > 0)
-	// {
-	// 	printf("fd: %d	%s\n", fd[2], line);
-	// 	free(line);
-	// }
-	// free(line);
-	// fd[3] = open("werther.txt", O_RDONLY);
-	// len = 6;
-	// while ((ret = get_next_line(fd[3], &line) > 0) && len-- > 0)
-	// {
-	// 	printf("fd: %d	%s\n", fd[3], line);
-	// 	free(line);
-	// }
-	// free(line);
 	close(fd[0]);
-	// close(fd[1]);
-	// close(fd[2]);
-	// close(fd[3]);
-	// while (1);
 }
